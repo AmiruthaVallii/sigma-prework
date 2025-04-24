6d53ecc9262b2eec70357855b40d2bc27265c68e
Ami Karthi Kumar <amu_kv@yahoo.co.uk>
2025-04-24 23:17:51 +0100

    Saving initial code

---
+++ age.py
@@ -0,0 +1,4 @@
+# When given a date, calculate the age between the current date and the given date.
+# The function should return the age in years only.
+
+def calculate_age(date):
---
+++ maxmin.py
@@ -0,0 +1,3 @@
+# Given a list of integers, return the highest and lowest numbers in the array (without using max() or min())
+
+def max_min(integer_list):
